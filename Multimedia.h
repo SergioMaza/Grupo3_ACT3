00c014d531c969aeaa4f19a864e043aded71d4c4
Sergio Maza <sergiomazahg@gmail.com>
2023-12-11 20:16:03 +0100

    Creadas las funciones de Multimedia de buscarTitulo, buscarCalidad y buscarGenero

--- Multimedia.h
+++ Multimedia.h
@@ -61,10 +61,9 @@ public:
     Multimedia(){};
 
     /**
-     * Metodo para busacar una multimedia a partir del titulo
+     * Metodo para buscar una multimedia a partir del titulo
      * @param titulo Titlo de la multimedia a buscar
      * @param mult Dynarray de las multimedias
-     * @return Multimedia con el titulo
      */
     void buscarTitulo(string titulo, Dynarray<Multimedia> &mult){
         bool encontrado;
@@ -79,6 +78,11 @@ public:
         }
     }
 
+    /**
+     * Metodo para buscar una multimedia a partir del genero
+     * @param genero Genero de la multimedia a buscar
+     * @param mult Dynarray de las multimedias
+     */
     void buscarGenero(GeneroMultimedia genero, Dynarray<Multimedia> &mult){
         bool encontrado;
         for(int i = 0; i < mult.capacity(); i++){
@@ -92,6 +96,11 @@ public:
         }
     }
 
+    /**
+     * Metodo para buscar una multimedia a partir de la calidad
+     * @param calidad Calidad de la multimedia a buscar
+     * @param mult Dynarray de las multimedias
+     */
     void buscarCalidad(Calidad calidad, Dynarray<Multimedia> &mult){
         bool encontrado;
         for(int i = 0; i < mult.capacity(); i++){
@@ -105,42 +114,82 @@ public:
         }
     }
 
+    /**
+     * Metodo para conseguir el titulo
+     * @return Titulo
+     */
     const string &getTitulo() const {
         return titulo;
     }
 
+    /**
+     * Metodo para conseguir el genero
+     * @return Genero
+     */
     GeneroMultimedia getGeneroMultimedia() const {
         return generoMultimedia;
     }
 
+    /**
+     * Metodo para conseguir la calidad
+     * @return Calidad
+     */
     Calidad getCalidad() const {
         return calidad;
     }
 
+    /**
+     * Metodo para sabe la disponibilidad de un archivo multimedia
+     * @return True -> Esta disponible | False -> No esta disponible
+     */
     bool isDispo() const {
         return dispo;
     }
 
+    /**
+     * Metodo para saber si la multimedia esta incluida o no con la tarifa basica
+     * @return True -> Esta incluida | False -> No esta incluida
+     */
     bool isIncluded() const {
         return included;
     }
 
+    /**
+     * Metodo para conseguir el precio extra
+     * @return Precio extra
+     */
     double getPrecioExtra() const {
         return precioExtra;
     }
 
+    /**
+     * Metodo para establecer la disponibilidad
+     * @param dispo Disponibilidad a establecer
+     */
     void setDispo(bool dispo) {
         Multimedia::dispo = dispo;
     }
 
+    /**
+     * Metodo para establecer si esta o no incluido
+     * @param included Si esta o no incluido en la tarifa basica
+     */
     void setIncluded(bool included) {
         Multimedia::included = included;
     }
 
+    /**
+     * Metodo para establecer el precio extra
+     * @param precioExtra Precio Extra
+     */
     void setPrecioExtra(double precioExtra) {
         Multimedia::precioExtra = precioExtra;
     }
 
+    /**
+     * Metodo toString de Multimedia
+     * @return toString de Multimedia
+     */
     string toString(){
         string dispoToString = (dispo) ? "Disponible" : "No disponible";
         string includedToString = (included) ? "Si" : "No";
@@ -153,6 +202,10 @@ public:
         ", Precio extra = " + to_string(precioExtra);
     }
 
+    /**
+     * Metodo para imprimir la multimedia en un Dynarray
+     * @param mult Array de multimedias
+     */
     void toStringformDynarray(Dynarray<Multimedia> &mult){
         for(int i = 0; i < mult.capacity(); i++){
             cout << mult.begin()[i].toString() << endl;
