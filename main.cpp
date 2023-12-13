c4c24775f748648abc0b61121c9cbb1ea672c5a7
VidhiSharma <vidhiav2611@gmail.com>
2023-12-13 18:40:25 +0100

    hemos creado las funciones en el Album.h pero fuera de la clase album. a su vez hemos creado la clase cancion con todos sus getter para poder acceder a sus variables mas adelante. Por ultimo hemos comentado todo el codigo

--- main.cpp
+++ main.cpp
@@ -9,25 +9,6 @@
 
 
 int main() {
-
-   cout << "Album" << endl;
-   cout << "---------------------" << endl;
-
-       auto *albumes = new Dynarray<Album>(19);
-       auto *canciones1 = new Dynarray<Cancion>(9);
-       auto *canciones2 = new Dynarray<Cancion>(9);
-       auto *cancion1 = new Cancion("Reina", "Mora");
-       auto *cancion2 = new Cancion("Como has estau?", "Mora");
-       auto *cancion3 = new Cancion("Hey mor", "Feid");
-       canciones1->insert(*cancion1);
-       canciones1->insert(*cancion2);
-       canciones2->insert(*cancion3);
-       auto *album1 = new Album("Estrella", Reggae, canciones1);
-       auto *album2 = new Album("Feli cumpleannos ferxxo", Reggae, canciones2);
-
-       albumes->insert(*album1);
-       albumes->insert(*album2);
-
     return 0;
 }
 
